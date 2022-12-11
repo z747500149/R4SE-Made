9f6f9534b6b9d7b9db6d2e925686e726579a7208
z747500149 <104322600+z747500149@users.noreply.github.com>
2022-12-11 14:51:55 -0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,3 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
 sed -i 's/Os/O3 -Wl,--gc-sections/g' include/target.mk
-sed -i 's/^[^#].*option ula/#&/' /etc/config/network
