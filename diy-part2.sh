6cc8c56cebed118cc03d0ffb658566018d05db42
z747500149 <104322600+z747500149@users.noreply.github.com>
2022-12-14 17:06:45 -0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
 sed -i 's/Os/O3 -Wl,--gc-sections/g' include/target.mk
+wget -qO - https://github.com/openwrt/openwrt/commit/8249a8c.patch | patch -p1
+wget -qO - https://github.com/openwrt/openwrt/commit/66fa343.patch | patch -p1
